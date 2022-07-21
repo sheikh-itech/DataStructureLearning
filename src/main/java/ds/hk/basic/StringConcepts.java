a344bbb4884573019e364c2d98adba844fe35aec
sheikh-itech <sheikh.itech123@gmail.com>
2022-07-21 23:28:46 +0530

    Added Externalization Concept

---
+++ src/main/java/ds/hk/basic/StringConcepts.java
@@ -0,0 +1,22 @@
+package ds.hk.basic;
+
+public class StringConcepts {
+
+	public static void main(String[] args) {
+
+		String []names = {"sheikh", "hapheej"};
+		String name = "sheikh";
+		System.out.println(names[0]==name);
+		
+		int []nums = {1,2,3,4,5};
+		Integer []nums1 = {1,2,3,4,5};
+		int num = 5;
+		System.out.println(nums[4]==num);
+		System.out.println(nums1[4]==num);
+		
+		String a = "a";
+		String ab = a+"b";
+		String abc = "abc";
+		System.out.println(ab==abc);
+	}
+}
--- src/main/java/ds/hk/strings/StringsDemo.java
+++ src/main/java/ds/hk/strings/StringsDemo.java
@@ -1,5 +1,10 @@
 package ds.hk.strings;
 
+/**
+ *	It can be used to return string from memory if it is created by a new keyword.
+ *	It creates an exact copy of the heap string object in the String Constant Pool 
+ */
+
 public class StringsDemo {
 
 	@SuppressWarnings("unused")
@@ -17,5 +22,22 @@ public class StringsDemo {
 		StringBuilder sb3 = new StringBuilder("abc");
 		StringBuilder sb4 = new StringBuilder();
 		sb4.append("abc");
+		
+		
+		String s21 = new String("abc").intern();
+		String s31 = new String("abc").intern();
+		System.out.println("s21==s31: "+(s21==s31));
+		System.out.println("new s2(abc): "+s21.hashCode());
+		System.out.println("new s3(abc): "+s31.hashCode());
+		
+		String str1 = "xyz";
+		String str2 = "xyz";	//This holds interned Object Reference
+		System.out.println("str1==str2: "+(str1==str2));
+		
+		String str3 = new String("xyz1");
+		String str4 = "xyz1";
+		System.out.println("new String(xyz1)==(xyz1): "+(str3==str4));
+		System.out.println("new String(xyz1) hash: "+str3.hashCode());
+		System.out.println("(xyz1) hash: "+str4.hashCode());
 	}
 }
